9df2dcc214eccc5067080b89e52a487de58fd3bf
Eina Safor Coma Bages <ecoma-ba@student.42barcelona.com>
2024-07-01 09:42:12 +0200

    en principi tot ok

--- tester_bonus.sh
+++ tester_bonus.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 for i in {0..100};
-	do cc -Wall -Wextra -Werror -g -c -I. main.c get_next_line_bonus.c get_next_line_utils_bonus.c -D BUFFER_SIZE=$(echo $i)LU
-	cc -Wall -Wextra -Werror -g -I. -o get_next_line_bonus main.o get_next_line_bonus.o get_next_line_utils_bonus.o -D BUFFER_SIZE=$(echo $i)LU
-	./get_next_line_bonus mytest;
+	do cc -Wall -Wextra -Werror -g -c -I. main_bonus.c get_next_line_bonus.c get_next_line_utils_bonus.c -D BUFFER_SIZE=$(echo $i)LU
+	cc -Wall -Wextra -Werror -g -I. -o get_next_line_bonus main_bonus.o get_next_line_bonus.o get_next_line_utils_bonus.o -D BUFFER_SIZE=$(echo $i)LU
+	./get_next_line_bonus mytest get_next_line_bonus.c;
 done
 for i in {0..100};
-	do cc -Wall -Wextra -Werror -g -c -I. main.c get_next_line_bonus.c get_next_line_utils_bonus.c -D BUFFER_SIZE=$(echo $i)00LU
-	cc -Wall -Wextra -Werror -g -I. -o get_next_line_bonus main.o get_next_line_bonus.o get_next_line_utils_bonus.o -D BUFFER_SIZE=$(echo $i)00LU
-	./get_next_line_bonus mytest;
+	do cc -Wall -Wextra -Werror -g -c -I. main_bonus.c get_next_line_bonus.c get_next_line_utils_bonus.c -D BUFFER_SIZE=$(echo $i)00LU
+	cc -Wall -Wextra -Werror -g -I. -o get_next_line_bonus main_bonus.o get_next_line_bonus.o get_next_line_utils_bonus.o -D BUFFER_SIZE=$(echo $i)00LU
+	./get_next_line_bonus mytest get_next_line_bonus.c;
 done
